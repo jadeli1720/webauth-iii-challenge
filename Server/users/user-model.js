f392007b3f9bc670d7df9aa475880d169e938ef7
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-18 15:53:01 -0600

    Added helper functions.

--- Server/users/user-model.js
+++ Server/users/user-model.js
@@ -1,5 +1,39 @@
 const db = require('../database/dbConfig.js');
 
 module.exports = {
-    
+    add,
+    find,
+    findBy,
+    findById
+};
+
+//RETURN ALWAYS within the functions
+
+//Find and Display user info (.select()) ---> only if user is verified 
+function find() {
+    return db('users')
+            .select('id', "username")//not password. We don't want that to show
+}
+
+//Used to add user and give them id
+function findById(id) {
+    return db('users')
+            .where({ id })
+            .first();
+}
+
+//User Registration ---> Add User
+function add(user) {
+    return db('users')
+            .insert(user, 'id')
+            .then(ids => {
+                const [id] = ids;
+                return findById(id)
+            })
+}
+
+//Used for Login ---> Find user by argument 
+function findBy(filter) {
+    return db('users')
+            .where(filter)
 }
